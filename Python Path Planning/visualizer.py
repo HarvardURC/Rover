a662eaf87004349349e5121debb169eb3d65fcb6
Albert Chien <albertchien@Alberts-MacBook-Pro-2.local>
2017-12-02 22:46:35 -0500

    basic path and visual

---
+++ Python Path Planning/visualizer.py
@@ -0,0 +1,58 @@
+import zlib
+import struct
+
+def map_to_array(d, size):
+	a = [[0 for x in range(size)] for y in range(size)] 
+	for key in d.keys():
+		x, y = key[0] + 64, 64 - key[1]
+		print x, y
+		a[y][x] = d[key]
+	return a
+
+def makeGrayPNG(data, height = None, width = None):
+    def I1(value):
+        return struct.pack("!B", value & (2**8-1))
+    def I4(value):
+        return struct.pack("!I", value & (2**32-1))
+    # compute width&height from data if not explicit
+    if height is None:
+        height = len(data) # rows
+    if width is None:
+        width = 0
+        for row in data:
+            if width < len(row):
+                width = len(row)
+    # generate these chunks depending on image type
+    makeIHDR = True
+    makeIDAT = True
+    makeIEND = True
+    png = b"\x89" + "PNG\r\n\x1A\n".encode('ascii')
+    if makeIHDR:
+        colortype = 0 # true gray image (no palette)
+        bitdepth = 8 # with one byte per pixel (0..255)
+        compression = 0 # zlib (no choice here)
+        filtertype = 0 # adaptive (each scanline seperately)
+        interlaced = 0 # no
+        IHDR = I4(width) + I4(height) + I1(bitdepth)
+        IHDR += I1(colortype) + I1(compression)
+        IHDR += I1(filtertype) + I1(interlaced)
+        block = "IHDR".encode('ascii') + IHDR
+        png += I4(len(IHDR)) + block + I4(zlib.crc32(block))
+    if makeIDAT:
+        raw = b""
+        for y in xrange(height):
+            raw += b"\0" # no filter for this scanline
+            for x in xrange(width):
+                c = b"\0" # default black pixel
+                if y < len(data) and x < len(data[y]):
+                    c = I1(data[y][x])
+                raw += c
+        compressor = zlib.compressobj()
+        compressed = compressor.compress(raw)
+        compressed += compressor.flush() #!!
+        block = "IDAT".encode('ascii') + compressed
+        png += I4(len(compressed)) + block + I4(zlib.crc32(block))
+    if makeIEND:
+        block = "IEND".encode('ascii')
+        png += I4(0) + block + I4(zlib.crc32(block))
+    return png
