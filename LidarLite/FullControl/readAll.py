5f8ab92eabba28787c28e6d5a875bd953aa3f73a
William Bryk <williambryk@college.harvard.edu>
2018-02-23 03:32:24 -0500

    editing full control, and deleted messed up accelerometer library folder name

--- LidarLite/FullControl/readAll.py
+++ LidarLite/FullControl/readAll.py
@@ -1,5 +1,12 @@
 from lidar_lite import Lidar_Lite
 
+import smbus
+import time
+
+# Get I2C buss
+bus = smbus.SMBus(1)
+bus.write_byte(0x40, 0xF5)
+
 lidar = Lidar_Lite()
 connected = lidar.connect(1)
 
