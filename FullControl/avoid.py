7e81aea8500e6475c5544f98c5a3a8d4c11dc692
maggiebasta <mbasta@college.harvard.edu>
2018-02-25 15:36:53 -0500

    avoidance algorithm

---
+++ FullControl/avoid.py
@@ -0,0 +1,40 @@
+import time
+import random
+import math
+from testLidarSweep import testLidarSweep
+
+class main():
+	def __init__(self):
+		self.sweepdegrees = LidarSweep.getData()[0]
+        self.lidarValues = LidarSweep.get_lidarValues()[1]
+        self.numVals = len(self.lidarValues)
+        self.center = math.ceil(self.numVals/2)
+        self.minDist = 1
+
+    # filter function to remove noise and irrelevent readings
+	def filter(self):
+		self.lidarValues = self.lidarValues
+
+	def turnDirection(self):
+		rl = [0,0]
+		best_index = armgmax([sum(lidarValues[:self.center]), sum(lidarValues[self.center:])])
+		rl[best_index] = 1
+		return rl
+
+	def control(self):
+		stable = True
+    	
+		# determine whether or not to turn
+		if self.lidarValues[self.center] < minDist:
+			self.stable = False
+
+		# determine turning until clear to go forward
+		while (stable == False):
+			right = self.turnDirection()[0]
+			left = self.turnDirection()[1]
+
+			if self.lidarValues[self.center] < minDist:
+				self.stable = True
+
+m = main()
+m.control()
