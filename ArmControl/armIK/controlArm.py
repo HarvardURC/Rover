5d360df8e846564c79e59273fdc35eaaebdd4407
William Bryk <williambryk@college.harvard.edu>
2018-02-25 06:37:19 -0500

    changed folder name of armIK

---
+++ ArmControl/armIK/controlArm.py
@@ -0,0 +1,138 @@
+import json
+import time
+import pygame
+import math
+
+wristPan = {
+"maxDegree": 87.0,
+"minDegree": -93.0,
+"homeDegree": 0.0,
+"maxpos": 600.0,
+"homepos": 380.0,
+"minpos": 130.0}
+
+wristTilt = {
+"maxDegree": 111.0,
+"minDegree": -45.0,
+"homeDegree": 0.0,
+"maxpos": 600.0,
+"homepos": 308.0,
+"minpos": 190.0}
+
+# continuous servo 
+continuousModes = {
+    "clockwise": 500,
+    "counterclockwise": 100,
+    "stop": 150
+}
+
+COMMANDS = {
+    "wristTilt" : wristTilt["homepos"],
+    "wristPan" : wristPan["homepos"],
+    "claw" : None,
+    "continuous" : None,
+    "l1Theta" : None,
+    "l2Theta" : None
+}
+
+
+
+def convertDegreeToPos(servo, newDegree):
+    if newDegree > servo["maxDegree"] or newDegree < servo["minDegree"]:
+        raise Exception("Cannot move servo to that value!")
+    
+    posOverDegreeRatio = (servo["maxpos"] - servo["minpos"])/float(servo["maxDegree"] - servo["minDegree"])
+
+    posDeltaFromMin = posOverDegreeRatio * float(newDegree - servo["minDegree"])
+
+    return posDeltaFromMin + servo["minpos"]
+
+#def convertPosToDegree(servo, newPos):
+    
+
+
+
+
+'''
+Gets joystick data and prints it
+'''
+pygame.init()
+#joystick.init()
+x = pygame.joystick.get_count()
+
+# if game controller is available, use game controller
+if x:
+    print "Using Gamepad for control"
+
+    j = pygame.joystick.Joystick(0)
+    j.init()
+    print ('Initialized Joystick : %s' % j.get_name())
+
+    which_toggle = ["LeftToggle_horizontal", "LeftToggle_vertical", "RightToggle_horizontal", "RightToggle_vertical",]
+    which_button = ["button_X", "button_A", "button_B", "button_Y", "button_LB", "button_RB", "button_LT", "button_RT", "button_back", "button_start", "button_toggleleft", "button_toggleright"]
+    try:
+        while True:
+            pygame.event.pump()
+
+
+            # read input from the two joysticks  
+                 
+            for i in range(0, j.get_numaxes()):
+                # make sure toggle is past a certain cutoff angle
+                if abs(j.get_axis(i)) >= 0.07:
+                    print (which_toggle[i], j.get_axis(i), i)
+                    
+                    file.seek(0)
+            
+            for i in range(0, j.get_numbuttons()):
+                if j.get_button(i) != 0:
+                    print (which_button[i], "button Joystick Num:", i)
+
+                    # write to controlValues.txt which button I'm pressing
+                    file.seek(0)
+                    file.write(str(i))
+
+            time.sleep(.1)
+
+    except KeyboardInterrupt:
+        j.quit()
+        file.close()
+
+else :
+    print "Switched to keyboard arrow keys for control"
+    while True:
+        keydownEvents = pygame.event.get()
+        #heldDownKeys = pygame.key.get_pressed()
+        for event in keydownEvents:
+            if event.type == pygame.KEYDOWN:
+                # continuous
+                if event.key == pygame.K_z:
+                    COMMANDS["continuous"] = continuousModes["counterclockwise"]
+                elif event.key == pygame.K_c:
+                    COMMANDS["continuous"] = continuousModes["clockwise"]
+                else:
+                    COMMANDS["continuous"] = continuousModes["stop"]
+
+                # wrist
+                if event.key == pygame.K_w:
+                    COMMANDS["wristTilt"] = COMMANDS["wristTilt"] + 3
+                elif event.key == pygame.K_s:
+                    COMMANDS["wristTilt"] = COMMANDS["wristTilt"] - 3
+                elif event.key == pygame.K_a:
+                    COMMANDS["wristPan"] = COMMANDS["wristPan"] + 3
+                elif event.key == pygame.K_d:
+                    COMMANDS["wristPan"] = COMMANDS["wristPan"] - 3
+
+                if event.key == pygame.K_o:
+                    COMMANDS["claw"] = "OPEN"
+                elif event.key == pygame.K_p:
+                    COMMANDS["claw"] = "CLOSE"
+
+        with open('data.txt', 'w') as outfile: 
+            print(COMMANDS) 
+            json.dump(COMMANDS, outfile)
+        
+        time.sleep(.2)
+    
+
+    
---
+++ ArmControl/armIK/moveArmServos.py
@@ -0,0 +1,86 @@
+# Simple demo of of the PCA9685 PWM servo/LED controller library.
+# This will move channel 0 from min to max position repeatedly.
+# Author: Tony DiCola
+# License: Public Domain
+from __future__ import division
+import actuatorHelpers as aH
+import time
+import math
+import RPi.GPIO as GPIO
+import json
+
+# Import the PCA9685 module.
+import Adafruit_PCA9685
+
+# Initialise the PCA9685 using the default address (0x40).
+pwm = Adafruit_PCA9685.PCA9685()
+
+# Helper function to make setting a servo pulse width simpler.
+def set_servo_pulse(channel, pulse):
+    pulse_length = 1000000    # 1,000,000 us per second
+    pulse_length //= 60       # 60 Hz
+    print('{0}us per period'.format(pulse_length))
+    pulse_length //= 4096     # 12 bits of resolution
+    print('{0}us per bit'.format(pulse_length))
+    pulse *= 1000
+    pulse //= pulse_length
+    pwm.set_pwm(channel, 0, pulse)
+
+# Set frequency to 60hz, good for servos.
+pwm.set_pwm_freq(60)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+
+
+
+# GET PINS
+(CONTINUOUSPIN, L1PIN, L2PIN, WRISTPANPIN, WRISTTILTPIN) = (0,1,2,3,4)
+
+'''
+COMMANDS = {
+    "wristTilt" : None,
+    "wristPan" : None,
+    "claw" : None,
+    "continuous" : None,
+    "l1Theta" : None,
+    "l2Theta" : None
+}
+'''
+
+def getCommands():
+    with open('data.txt') as json_file:  
+        data = json.load(json_file)
+    return data
+
+while True:
+    # GET SERVO COMMANDS
+    COMMANDS = getCommands()
+
+    # MOVE SERVOS
+    if COMMANDS["wristTilt"]:
+        pwm.set_pwm(WRISTTILTPIN, 0, COMMANDS["wristTilt"])
+    if COMMANDS["wristPan"]:
+        pwm.set_pwm(WRISTPANPIN, 0, COMMANDS["wristTilt"])
+    if COMMANDS["claw"]:
+        if COMMANDS["claw"] == "OPEN":
+            GPIO.output(19, False)
+            GPIO.output(16, True)
+        elif COMMANDS["claw"] == "CLOSE":
+            GPIO.output(19, True)
+            GPIO.output(16, False)
+    else:
+        GPIO.output(19, False)
+        GPIO.output(16, False)
+
+    if COMMANDS["l1Theta"]:
+        l1DesPos, l2DesPos = aH.getActuatorPosFromThetas(COMMANDS["l1Theta"], COMMANDS["l1Theta"])
+        pwm.set_pwm(L1PIN, 0, l1DesPos)
+    if COMMANDS["l2Theta"]:
+        l1DesPos, l2DesPos = aH.getActuatorPosFromThetas(COMMANDS["l2Theta"], COMMANDS["l2Theta"])
+        pwm.set_pwm(L2PIN, 0, l2DesPos)
+    if COMMANDS["continuous"]:
+        pwm.set_pwm(CONTINUOUSPIN, 0, COMMANDS["continuous"])
+
+    time.sleep(.3)
