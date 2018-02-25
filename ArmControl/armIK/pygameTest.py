7512c53483ba9bfb6b3d291af85f076cc017203c
William Bryk <williambryk@college.harvard.edu>
2018-02-25 07:32:45 -0500

    forgot to add screen

--- ArmControl/armIK/pygameTest.py
+++ ArmControl/armIK/pygameTest.py
@@ -8,6 +8,7 @@ Gets joystick data and prints it
 pygame.init()
 #joystick.init()
 x = pygame.joystick.get_count()
+screen = pygame.display.set_mode((400, 300))
 
 # if game controller is available, use game controller
 if x:
